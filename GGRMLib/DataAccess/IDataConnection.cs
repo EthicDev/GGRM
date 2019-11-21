9916787e6691b03ce217c04865576cba54890678
Mack <macklem.curtis@gmail.com>
2019-11-21 12:35:24 -0500

    Added more Customer SQL methods

--- GGRMLib/DataAccess/IDataConnection.cs
+++ GGRMLib/DataAccess/IDataConnection.cs
@@ -1,6 +1,7 @@
 ﻿using GGRMLib.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,11 @@ namespace GGRMLib.DataAccess
         CustomerOrder CreateCO(CustomerOrder co, out string status);
 
         Customer CreateCustomer(Customer cust, out string status);
+
+        List<Customer> GetCustomersList(out string status);
+
+        DataTable GetCustomersDataTable(out string status);
+
+        Customer GetCustomerByID(int id, out string status);
     }
 }
--- GGRMLib/DataAccess/SqlConnector.cs
+++ GGRMLib/DataAccess/SqlConnector.cs
@@ -9,8 +9,9 @@ using GGRMLib.Models;
 
 namespace GGRMLib.DataAccess
 {
-    class SqlConnector : IDataConnection
+    public class SqlConnector : IDataConnection
     {
+        //CustomerOrder Methods
         public CustomerOrder CreateCO(CustomerOrder co, out string status)
         {
             status = "CustomerOrder creation failed.";
@@ -23,6 +24,7 @@ namespace GGRMLib.DataAccess
             return co;
         }
 
+        //CustomerOrderLine Methods
         public CustomerOrderLine CreateCOLine(CustomerOrderLine col, out string status)
         {
             status = "COLine creation failed.";
@@ -33,6 +35,7 @@ namespace GGRMLib.DataAccess
             return col;
         }
 
+        //Customer Methods
         public Customer CreateCustomer(Customer cust, out string status)
         {
             status = "Customer insertion failed.";
@@ -57,8 +60,94 @@ namespace GGRMLib.DataAccess
                         cust.ID = records.GetInt32(0);
                     }
                 }
-                status = "Customer insertion successful.";
+                status = "Customer insertion succeeded.";
             } catch (Exception ex) { status = ex.Message; }
+            return cust;
+        }
+
+        public List<Customer> GetCustomersList(out string status)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            status = "Getting customers failed.";
+            try
+            {
+                using (IDbConnection connection = new SqlConnection(GlobalConfig.ConString("GGRM")))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = (SqlConnection)connection;
+                    cmd.CommandText = "SELECT id, custFirst, custLast, custPhone, custAddress, custCity, custPostal, custEmail FROM customer ORDER BY custLast";
+                    SqlDataReader records = cmd.ExecuteReader();
+                    while (records.Read())
+                    {
+                        Customer cust = new Customer(records.GetString(1),
+                            records.GetString(2),
+                            records.GetString(3),
+                            records.GetString(4),
+                            records.GetString(5),
+                            records.GetString(6),
+                            records.GetString(7));
+                        cust.ID = records.GetInt32(0);
+                        customers.Add(cust);
+                    }
+                }
+                status = "Getting customers succeeded.";
+            }
+            catch (Exception ex) { status = ex.Message; }
+
+            return customers;
+        }
+
+        public DataTable GetCustomersDataTable(out string status)
+        {
+            DataTable dtCustomers = new DataTable();
+
+            status = "Getting customers failed.";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(GlobalConfig.ConString("GGRM")))
+                {
+                    conn.Open();
+                    string sqlCommand = "SELECT id, custLast+', '+custFirst AS Name, custPhone AS Phone, custAddress AS Address, custCity AS City, custPostal AS [Postal Code], custEmail AS Email FROM customer ORDER BY custLast";
+                    SqlDataAdapter sqlDa = new SqlDataAdapter(sqlCommand, conn);
+                    sqlDa.Fill(dtCustomers);
+                }
+                status = "Getting customers succeeded.";
+            }
+            catch (Exception ex) { status = ex.Message; }
+
+            return dtCustomers;
+        }
+        public Customer GetCustomerByID(int id, out string status)
+        {
+            Customer cust = new Customer();
+            status = "Getting customer failed.";
+            try
+            {
+                using (IDbConnection connection = new SqlConnection(GlobalConfig.ConString("GGRM")))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = (SqlConnection)connection;
+                    cmd.CommandText = "SELECT id, custFirst, custLast, custPhone, custAddress, custCity, custPostal, custEmail FROM customer WHERE id = " + id.ToString();
+                    SqlDataReader records = cmd.ExecuteReader();
+                    if (records.Read())
+                    {
+                        cust = new Customer(records.GetString(1),
+                            records.GetString(2),
+                            records.GetString(3),
+                            records.GetString(4),
+                            records.GetString(5),
+                            records.GetString(6),
+                            records.GetString(7));
+                        cust.ID = records.GetInt32(0);
+                    }
+                }
+                status = "Getting customer succeeded.";
+            }
+            catch (Exception ex) { status = ex.Message; }
+
             return cust;
         }
     }
