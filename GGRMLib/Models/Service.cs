117758d82d4108915e462d67735162d40b4dce45
Mack <macklem.curtis@gmail.com>
2019-12-03 13:16:48 -0500

    Service+ServiceOrder Models

---
+++ GGRMLib/Models/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGRMLib.Models
+{
+    public class Service
+    {
+        public int ID { get; set; }
+
+        public int SerName { get; set; }
+
+        public int SerDescription { get; set; }
+
+        public int SerPrice { get; set; }
+    }
+}
---
+++ GGRMLib/Models/ServiceOrder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGRMLib.Models
+{
+    public class ServiceOrder
+    {
+        public ServiceOrder()
+        {
+            ID = -1;
+        }
+
+        public int ID { get; set; }
+
+        public DateTime SerOrdDateIn { get; set; }
+
+        public DateTime SerOrdDateOut { get; set; }
+
+        public string SerOrdIssue { get; set; }
+
+        public bool SerOrdWarranty { get; set; }
+
+        public string SerOrdStatus { get; set; }
+
+        public int CustOrdID { get; set; }
+
+        public int ServiceID { get; set; }
+
+        public int EquipID { get; set; }
+
+        public int EmpID { get; set; }
+    }
+}
